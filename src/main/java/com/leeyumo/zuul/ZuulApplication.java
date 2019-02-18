2ebf3cb6cc21c3572bf6ebd50d2341098031ccce
leeyumo <106108035@qq.com>
2019-02-18 17:06:41 +0800

    firstCommit

---
+++ src/main/java/com/leeyumo/zuul/ZuulApplication.java
@@ -0,0 +1,35 @@
+package com.leeyumo.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ZuulApplication {
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 允许任何域名使用
+        corsConfiguration.addAllowedHeader("*"); // 允许任何头
+        corsConfiguration.addAllowedMethod("*"); // 允许任何方法（post、get等）
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/appH5/**", buildConfig()); // 对接口配置跨域设置
+        return new CorsFilter(source);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
---
+++ src/main/java/com/leeyumo/zuul/controller/ErrorHandlerController.java
@@ -0,0 +1,65 @@
+package com.leeyumo.zuul.controller;
+
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+@RestController
+public class ErrorHandlerController implements ErrorController {
+ 
+    /**
+     * 出异常后进入该方法，交由下面的方法处理
+     */
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+ 
+    @RequestMapping("/error")
+    public String error(){
+        RequestContext ctx = RequestContext.getCurrentContext();
+        ZuulException exception = (ZuulException)ctx.getThrowable();
+        return "{\"code\" : 500, \"msg\" : \"网关异常\", \"error\" : \""+getStackTraceInfo(exception)+"\"}";
+    }
+    
+    /**
+     * 获取e.printStackTrace() 的具体信息，赋值给String 变量，并返回
+     * 
+     * @param e
+     *            Exception
+     * @return e.printStackTrace() 中 的信息
+     */
+    public static String getStackTraceInfo(Exception e) {
+        StringWriter sw = null;
+        PrintWriter pw = null;
+        try {
+            sw = new StringWriter();
+            pw = new PrintWriter(sw);
+            e.printStackTrace(pw);//将出错的栈信息输出到printWriter中
+            pw.flush();
+            sw.flush();
+            return sw.toString();
+        } catch (Exception ex) {
+            return "发生错误";
+        } finally {
+            if (sw != null) {
+                try {
+                    sw.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            if (pw != null) {
+                pw.close();
+            }
+        }
+    }
+}
+ 
+
